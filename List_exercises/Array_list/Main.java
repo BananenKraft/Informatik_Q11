2a2336f75e063a1c23e894767f120654619bc667
BananenKraft <mikakoerner@outlook.de>
2022-09-19 14:02:14 -0400

    Remove Tests from Main.java and Create proper README.md

--- List_exercises/Array_list/Main.java
+++ List_exercises/Array_list/Main.java
@@ -2,11 +2,5 @@
 class Main {
     public static void main(String[] args) {
         MyListArray myList = new MyListArray();
-         
-        myList.push(new Human("Mika", 15));
-        myList.push(new Human("Sarah", 16));
-        myList.push(new Human("Blomi", 16)); 
-        myList.humatAtIndex(0, true);
-        
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Informatik_Q11
+
+A collection of Exercises and Solutions for 1inf1 held by Mr.Wechsler.
+These inculde most of the given exercices as well as some uncompulsory ideas.
+Every exercice has been completed without looking at any given solution,
+except code snippets surrounded by  /*s*/ code /*!s*/
